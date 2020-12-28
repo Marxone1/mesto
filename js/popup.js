f23ee0eed40475f71624a9338240bfe02ed34f60
Marxone1 <73601226+Marxone1@users.noreply.github.com>
2020-12-28 12:48:11 +0300

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector(".popup");
+let popupForm = popup.querySelector(".popup__container-form");
 let openPopupButton = document.querySelector(".profile__info-edit-button");
 let closePopupButton = popup.querySelector(".popup__container-close-button");
-let popupSubmitButton = popup.querySelector(".popup__container-form-save-button");
 let userName = document.querySelector(".profile__info-username");
 let profession = document.querySelector(".profile__info-profession");
-let inputUsername = popup.querySelector("[name = 'username']");
-let inputProfession = popup.querySelector("[name = 'profession']")
+let inputUsername = popup.querySelector(".popup__container-form-input-username_text_username");
+let inputProfession = popup.querySelector(".popup__container-form-input-username_text_profession");
 function openPopup (){
     inputUsername.value = userName.textContent;
     inputProfession.value = profession.textContent;
@@ -15,11 +15,11 @@ function closePopup (){
     popup.classList.remove("popup__opened");
 }
 function submitPopup (event){
-    event.preventDefault()
+    event.preventDefault();
     userName.textContent = inputUsername.value;
     profession.textContent = inputProfession.value;
-    closePopup()
+    closePopup();
 }
 openPopupButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
-popupSubmitButton.addEventListener("click", submitPopup)
+popupForm.addEventListener("submit", submitPopup)
