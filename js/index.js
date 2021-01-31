42ac8385459fc63ea1460256e13f1672e5c62b92
Marxone1 <73601226+Marxone1@users.noreply.github.com>
2021-01-31 21:13:39 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -24,13 +24,13 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
-const addPlace = document.querySelector(".add-place");
+const addPlace = document.getElementById("addPlace");
 const addPlaceForm = addPlace.querySelector(".add-place__container-form");
 const openAddPlaceButton = document.querySelector(".profile__add-button");
 const closeAddPlaceButton = addPlace.querySelector(".add-place__container-close-button");
 const elements = document.querySelector('.elements');
-const popup = document.querySelector(".popup");
-const popupForm = popup.querySelector(".popup__container-form");
+const popup = document.getElementById("popup");
+const popupForm = popup;
 const openPopupButton = document.querySelector(".profile__info-edit-button");
 const closePopupButton = popup.querySelector(".popup__container-close-button");
 const userName = document.querySelector(".profile__info-username");
@@ -38,7 +38,7 @@ const profession = document.querySelector(".profile__info-profession");
 const inputUsername = popup.querySelector(".popup__container-form-input_text_username");
 const inputProfession = popup.querySelector(".popup__container-form-input_text_profession");
 const imageCloseButton = document.querySelector('.image-popup__close-button');
-const imagePopup = document.querySelector('.image-popup');
+const imagePopup = document.getElementById("imagePopup");
 
 function addCard( elements, cardElement){
     elements.prepend(cardElement);
@@ -50,12 +50,14 @@ function createCard(name, link){
   const imageElement = cardTemplate.querySelector('.elements__item-image')
   imageElement.addEventListener("click", () => {
     imagePopup.querySelector('.image-popup__description').textContent = name;
+    imagePopup.querySelector('.image-popup__image').alt = 'здесь должна быть картинка';
     imagePopup.querySelector('.image-popup__image').src = link;
-    open(imagePopup);
+    openPopup(imagePopup);
   })
   deleteElement.addEventListener("click", deleteListener);
   likeElement.addEventListener("click", likeListener);
   cardTemplate.querySelector('.elements__item-description-title').textContent = name;
+  cardTemplate.querySelector('.elements__item-image').alt = 'здесь должна быть картинка';
   cardTemplate.querySelector('.elements__item-image').src = link;
   return cardTemplate;
 }
@@ -65,7 +67,7 @@ function submitAddPlace (event){
   const name = addPlaceForm.querySelector('.add-place__container-form-input_text_name').value;
   const link = addPlaceForm.querySelector('.add-place__container-form-input_text_link').value;
   addCard(elements, createCard(name, link));
-  close(addPlace);
+  closePopup(addPlace);
   event.target.reset();
 }
 function likeListener(event){
@@ -76,35 +78,34 @@ function deleteListener(event){
   event.target.closest('.card').remove();
 }
 
-function submitPopup (event){
+function submitProfilePopup (event){
   event.preventDefault();
   userName.textContent = inputUsername.value;
   profession.textContent = inputProfession.value;
-  close(popup);
+  closePopup(popup);
 }
 
-function open(popup){
+function openPopup(popup){
   popup.classList.add("opened")
 }
-function close(popup){
-  popup.classList.add("close");
-  popup.classList.remove("opened");
-  setTimeout(() => popup.classList.remove("close"), 500)/**извините, не нашел других способов сделать это*/       
+//подскажите пожалуйста, как избавиться от появления попапов при загрузке
+function closePopup(popup){
+  popup.classList.remove("opened");  
 }
 
 initialCards.forEach(element => addCard(elements, createCard(element.name, element.link)))
 
 addPlaceForm.addEventListener('submit', submitAddPlace);
 
-popupForm.addEventListener("submit", submitPopup)
+popupForm.addEventListener("submit", submitProfilePopup)
 
-openAddPlaceButton.addEventListener("click", () => open(addPlace));
+openAddPlaceButton.addEventListener("click", () => openPopup(addPlace));
 openPopupButton.addEventListener("click", () => {
   inputUsername.value = userName.textContent;
   inputProfession.value = profession.textContent;
-  open(popup);
+  openPopup(popup);
 });
 
-imageCloseButton.addEventListener("click", () => close(imagePopup))
-closeAddPlaceButton.addEventListener("click", () => close(addPlace));
-closePopupButton.addEventListener("click", () => close(popup));
+imageCloseButton.addEventListener("click", () => closePopup(imagePopup))
+closeAddPlaceButton.addEventListener("click", () => closePopup(addPlace));
+closePopupButton.addEventListener("click", () => closePopup(popup));
