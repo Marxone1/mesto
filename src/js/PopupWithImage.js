cd789d3eda245831d8aef74fdb691e3f73b8ddc9
Marxone1 <73601226+Marxone1@users.noreply.github.com>
2021-09-01 22:34:49 +0300

    Add files via upload

--- src/js/PopupWithImage.js
+++ src/js/PopupWithImage.js
@@ -6,7 +6,7 @@ export class PopupWithImage extends Popup{
         this._image = this._popup.querySelector('.popup__image')
     }
     
-    _open(name, link){
+    open(name, link){
         this._popup.querySelector('.popup__description').textContent = name;
         this._image.src = link
         this._image.alt = name
